93774cc6c8125b615521e99735def61e33ee4711
PhantomPilot <none>
2026-04-05 00:08:12 -0400

    Forgot to add Cusack as option

--- scripts/AutoFarmers.py
+++ scripts/AutoFarmers.py
@@ -110,7 +110,7 @@ Tune your gear so you can guarantee that.<br>
     """,
     "Dogs Floor 4": """
 <p><strong>Requirements:</strong><br>
-• Escalin, Lillia/Roxy(recommended), Nasiens, Thonar</p>
+• Escalin, Lillia/Cusack/Roxy(recommended), Nasiens, Thonar</p>
     """,
     "Dogs Farmer": """
 <p><strong>Requirements:</strong><br>
